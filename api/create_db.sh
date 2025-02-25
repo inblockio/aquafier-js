bc0a7dcd7737f2b6b9f2fb5bbac39f1e9bf0217d
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-25 13:02:10 +0300

    DB creation commands

---
+++ api/create_db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Start PostgreSQL command line interface
+echo "Starting PostgreSQL command line interface..."
+sudo -u postgres psql <<EOF
+
+CREATE DATABASE aquafier_js;
+CREATE USER aquafier_js WITH ENCRYPTED PASSWORD 'aquafier_js';
+GRANT ALL PRIVILEGES ON DATABASE aquafier_js TO aquafier_js;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO aquafier_js;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO aquafier_js;
+GRANT postgres TO aquafier_js;
+
+EOF
+
+# Exit PostgreSQL command line interface
+echo "Exiting PostgreSQL command line interface..."
