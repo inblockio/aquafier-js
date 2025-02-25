ca48abb3025d6f2b1b5f207460d9079c88fd16f6
arthur-kamau <kamaukenn11@gmail.com>
2025-02-25 08:32:51 +0300

    prisma and more logic

---
+++ api/prisma/migrations/20250225053056_init/migration.sql
@@ -0,0 +1,183 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "user" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user")
+);
+
+-- CreateTable
+CREATE TABLE "Contract" (
+    "hash" TEXT NOT NULL,
+    "latest" JSONB NOT NULL,
+    "sender" TEXT NOT NULL,
+    "receiver" TEXT NOT NULL,
+    "option" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+    "userUser" TEXT,
+
+    CONSTRAINT "Contract_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Latest" (
+    "hash" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "revisionHash" TEXT,
+
+    CONSTRAINT "Latest_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Revision" (
+    "hash" TEXT NOT NULL,
+    "user" TEXT NOT NULL,
+    "nonce" TEXT NOT NULL,
+    "shared" JSONB NOT NULL,
+    "contract" JSONB NOT NULL,
+    "previous" TEXT,
+    "children" JSONB NOT NULL,
+    "local_timestamp" TIMESTAMP(3) NOT NULL,
+    "Revision_type" TEXT NOT NULL,
+    "Verification_leaves" JSONB NOT NULL,
+
+    CONSTRAINT "Revision_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "hash" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "file_hash" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Link" (
+    "hash" TEXT NOT NULL,
+    "link_type" TEXT NOT NULL,
+    "link_require_indepth_verification" BOOLEAN NOT NULL,
+    "link_verification_hash" TEXT,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Index" (
+    "hash" TEXT NOT NULL,
+    "file_hash" TEXT NOT NULL,
+    "uri" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "Index_pkey" PRIMARY KEY ("hash","file_hash")
+);
+
+-- CreateTable
+CREATE TABLE "Signature" (
+    "hash" TEXT NOT NULL,
+    "signature_digest" TEXT NOT NULL,
+    "signature_wallet_address" TEXT NOT NULL,
+    "signature_type" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "Signature_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Witness" (
+    "hash" TEXT NOT NULL,
+    "Witness_merkle_root" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "Witness_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "WitnessEvent" (
+    "Witness_merkle_root" TEXT NOT NULL,
+    "Witness_timestamp" TIMESTAMP(3) NOT NULL,
+    "Witness_network" TEXT NOT NULL,
+    "Witness_smart_contract_address" TEXT NOT NULL,
+    "Witness_transaction_hash" TEXT NOT NULL,
+    "Witness_sender_account_address" TEXT NOT NULL,
+
+    CONSTRAINT "WitnessEvent_pkey" PRIMARY KEY ("Witness_merkle_root")
+);
+
+-- CreateTable
+CREATE TABLE "MerkleNodes" (
+    "node_hash" TEXT NOT NULL,
+    "parent_hash" TEXT,
+    "height" INTEGER NOT NULL,
+    "is_leaf" BOOLEAN NOT NULL,
+    "left_child_hash" TEXT,
+    "right_child_hash" TEXT,
+    "witnessHash" TEXT,
+    "witnessEventWitnessMerkleRoot" TEXT,
+
+    CONSTRAINT "MerkleNodes_pkey" PRIMARY KEY ("node_hash")
+);
+
+-- CreateTable
+CREATE TABLE "AquaForms" (
+    "hash" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" JSONB NOT NULL,
+    "type" TEXT NOT NULL,
+    "reference_count" INTEGER NOT NULL,
+
+    CONSTRAINT "AquaForms_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Settings" (
+    "user_pub_key" TEXT NOT NULL,
+    "cli_pub_key" TEXT NOT NULL,
+    "cli_priv_key" TEXT NOT NULL,
+    "Witness_network" TEXT NOT NULL,
+    "Witness_contract_address" TEXT NOT NULL,
+    "theme" TEXT NOT NULL,
+
+    CONSTRAINT "Settings_pkey" PRIMARY KEY ("user_pub_key")
+);
+
+-- AddForeignKey
+ALTER TABLE "Contract" ADD CONSTRAINT "Contract_userUser_fkey" FOREIGN KEY ("userUser") REFERENCES "User"("user") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Latest" ADD CONSTRAINT "Latest_user_fkey" FOREIGN KEY ("user") REFERENCES "User"("user") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Latest" ADD CONSTRAINT "Latest_revisionHash_fkey" FOREIGN KEY ("revisionHash") REFERENCES "Revision"("hash") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_hash_fkey" FOREIGN KEY ("hash") REFERENCES "Revision"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Link" ADD CONSTRAINT "Link_hash_fkey" FOREIGN KEY ("hash") REFERENCES "Revision"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Index" ADD CONSTRAINT "Index_file_hash_fkey" FOREIGN KEY ("file_hash") REFERENCES "File"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Signature" ADD CONSTRAINT "Signature_hash_fkey" FOREIGN KEY ("hash") REFERENCES "Revision"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Witness" ADD CONSTRAINT "Witness_hash_fkey" FOREIGN KEY ("hash") REFERENCES "Revision"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Witness" ADD CONSTRAINT "Witness_Witness_merkle_root_fkey" FOREIGN KEY ("Witness_merkle_root") REFERENCES "WitnessEvent"("Witness_merkle_root") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MerkleNodes" ADD CONSTRAINT "MerkleNodes_witnessHash_fkey" FOREIGN KEY ("witnessHash") REFERENCES "Witness"("hash") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MerkleNodes" ADD CONSTRAINT "MerkleNodes_witnessEventWitnessMerkleRoot_fkey" FOREIGN KEY ("witnessEventWitnessMerkleRoot") REFERENCES "WitnessEvent"("Witness_merkle_root") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AquaForms" ADD CONSTRAINT "AquaForms_hash_fkey" FOREIGN KEY ("hash") REFERENCES "Revision"("hash") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Settings" ADD CONSTRAINT "Settings_user_pub_key_fkey" FOREIGN KEY ("user_pub_key") REFERENCES "User"("user") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ api/schema/Aquafier_DB.sql
@@ -0,0 +1,140 @@
+CREATE TABLE "User" (
+  "user" pubkey PRIMARY KEY
+);
+
+CREATE TABLE "Contract" (
+  "hash" hash PRIMARY KEY,
+  "latest" array,
+  "sender" pubkey,
+  "receiver" pubkey,
+  "option" string,
+  "reference_count" int
+);
+
+CREATE TABLE "Latest" (
+  "hash" hash PRIMARY KEY,
+  "user" pubkey
+);
+
+CREATE TABLE "Revision" (
+  "hash" hash PRIMARY KEY,
+  "user" pubkey,
+  "nonce" string,
+  "shared" array_pubkey,
+  "contract" array,
+  "previous" varchar,
+  "children" hash_map,
+  "local_timestamp" timestamp,
+  "Revision_type" string,
+  "Verification_leaves" hash_map
+);
+
+CREATE TABLE "File" (
+  "hash" hash PRIMARY KEY,
+  "content" utf8_content,
+  "file_hash" hash,
+  "reference_count" int
+);
+
+CREATE TABLE "Link" (
+  "hash" hash PRIMARY KEY,
+  "link_type" string,
+  "link_require_indepth_verification" boolean,
+  "link_verification_hash" hash_map,
+  "reference_count" int
+);
+
+CREATE TABLE "Index" (
+  "hash" array,
+  "file_hash" hash,
+  "uri" URLPath_and_Title,
+  "reference_count" int,
+  PRIMARY KEY ("hash", "file_hash")
+);
+
+CREATE TABLE "Signature" (
+  "hash" hash PRIMARY KEY,
+  "signature_digest" string,
+  "signature_wallet_address" varchar,
+  "signature_type" hash,
+  "reference_count" int
+);
+
+CREATE TABLE "Witness" (
+  "hash" hash PRIMARY KEY,
+  "Witness_merkle_root" hash,
+  "reference_count" int
+);
+
+CREATE TABLE "WitnessEvent" (
+  "Witness_merkle_root" hash PRIMARY KEY,
+  "Witness_timestamp" timestamp,
+  "Witness_network" chain_id,
+  "Witness_smart_contract_address" hash,
+  "Witness_transaction_hash" tx_hash,
+  "Witness_sender_account_address" pubkey
+);
+
+CREATE TABLE "MerkleNodes" (
+  "node_hash" TEXT,
+  "parent_hash" TEXT,
+  "height" INTEGER,
+  "is_leaf" BOOLEAN,
+  "left_child_hash" TEXT,
+  "right_child_hash" TEXT
+);
+
+CREATE TABLE "AquaForms" (
+  "hash" hash PRIMARY KEY,
+  "key" string,
+  "value" object,
+  "type" string,
+  "reference_count" int
+);
+
+CREATE TABLE "Settings" (
+  "user_pub_key" pubkey PRIMARY KEY,
+  "cli_pub_key" pubkey,
+  "cli_priv_key" private_key,
+  "Witness_network" chain_id,
+  "Witness_contract_address" hash,
+  "theme" string
+);
+
+ALTER TABLE "Latest" ADD FOREIGN KEY ("hash") REFERENCES "User" ("user");
+
+ALTER TABLE "Settings" ADD FOREIGN KEY ("user_pub_key") REFERENCES "User" ("user");
+
+ALTER TABLE "Contract" ADD FOREIGN KEY ("hash") REFERENCES "User" ("user");
+
+ALTER TABLE "Revision" ADD FOREIGN KEY ("hash") REFERENCES "Latest" ("hash");
+
+ALTER TABLE "File" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
+
+ALTER TABLE "Signature" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
+
+ALTER TABLE "Witness" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
+
+ALTER TABLE "Link" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
+
+ALTER TABLE "Contract" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
+
+ALTER TABLE "Index" ADD FOREIGN KEY ("hash") REFERENCES "File" ("hash");
+
+ALTER TABLE "Link" ADD FOREIGN KEY ("link_verification_hash") REFERENCES "Index" ("hash");
+
+ALTER TABLE "Revision" ADD FOREIGN KEY ("hash") REFERENCES "Index" ("file_hash");
+
+ALTER TABLE "Witness" ADD FOREIGN KEY ("Witness_merkle_root") REFERENCES "WitnessEvent" ("Witness_merkle_root");
+
+ALTER TABLE "MerkleNodes" ADD FOREIGN KEY ("node_hash") REFERENCES "Witness" ("hash");
+
+ALTER TABLE "MerkleNodes" ADD FOREIGN KEY ("node_hash") REFERENCES "WitnessEvent" ("Witness_merkle_root");
+
+ALTER TABLE "MerkleNodes" ADD FOREIGN KEY ("node_hash") REFERENCES "MerkleNodes" ("parent_hash");
+
+ALTER TABLE "MerkleNodes" ADD FOREIGN KEY ("node_hash") REFERENCES "MerkleNodes" ("left_child_hash");
+
+ALTER TABLE "MerkleNodes" ADD FOREIGN KEY ("node_hash") REFERENCES "MerkleNodes" ("right_child_hash");
+
+ALTER TABLE "AquaForms" ADD FOREIGN KEY ("hash") REFERENCES "Revision" ("hash");
