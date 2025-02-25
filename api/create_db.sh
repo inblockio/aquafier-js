d1b7f16236d2fd67d9a5c9bbbc123cf7283ddb32
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-25 13:23:21 +0300

    UPdate create_db.sh to be able to work well with prisma

--- api/create_db.sh
+++ api/create_db.sh
@@ -5,13 +5,14 @@ echo "Starting PostgreSQL command line interface..."
 sudo -u postgres psql <<EOF
 
 CREATE DATABASE aquafier_js;
-CREATE USER aquafier_js WITH ENCRYPTED PASSWORD 'aquafier_js';
+CREATE USER aquafier_js WITH ENCRYPTED PASSWORD 'aquafier_js' CREATEDB;
 GRANT ALL PRIVILEGES ON DATABASE aquafier_js TO aquafier_js;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO aquafier_js;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO aquafier_js;
 GRANT postgres TO aquafier_js;
 
+ALTER USER aquafier_js WITH CREATEDB;
 EOF
 
 # Exit PostgreSQL command line interface
