040a3844b969db6126972df6c4be6746d2ea182e
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-03-26 16:02:24 +0300

    Update prisma schema to allow docker migrations Update file action to upload file on drop Update home component to hide everything till one is logged in

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,40 +1,56 @@
-import { Box, Container, VStack } from "@chakra-ui/react"
+import { Box, Center, Container, Stack, Text, VStack } from "@chakra-ui/react"
 import { FileUploadDropzone, FileUploadList, FileUploadRoot } from "../components/ui/file-button"
 import FilesTable from "../components/ui/table"
+import { useStore } from "zustand"
+import appStore from "../store"
+import ConnectWallet from "../components/ui/navigation/ConnectWallet"
 
 const Home = () => {
-
+    const { session } = useStore(appStore)
 
     return (
         <>
-            <Container fluid maxWidth={{ base: 'vw', md: '10/12' }} py={'14'} px={{ base: 1, md: 10 }}>
-                <VStack alignItems={'start'} gap={'10'}>
-                    <FileUploadRoot borderRadius={'2xl'} alignItems="stretch" maxFiles={10} cursor={'pointer'} >
-                        <FileUploadDropzone
-                            borderRadius={'2xl'}
-                            label="Drag and drop here to upload"
-                            description=".png, .jpg up to 20MB"
-                            _hover={{
-                                outline: "4px dashed",
-                                outlineOffset: '4px'
-                            }}
-                        />
-                        {/* 
+            {
+                session ? (
+                    <Container fluid maxWidth={{ base: 'vw', md: '10/12' }} py={'14'} px={{ base: 1, md: 10 }}>
+                        <VStack alignItems={'start'} gap={'10'}>
+                            <FileUploadRoot borderRadius={'2xl'} alignItems="stretch" maxFiles={10} cursor={'pointer'} >
+                                <FileUploadDropzone
+                                    borderRadius={'2xl'}
+                                    label="Drag and drop here to upload"
+                                    description=".png, .jpg up to 20MB"
+                                    _hover={{
+                                        outline: "4px dashed",
+                                        outlineOffset: '4px'
+                                    }}
+                                />
+                                {/* 
                             I have set clearable to false since when selecting new files. 
                             If the index is already in uploaed files array, then it marks it as uploaded. 
                             We should be able to fix this to avoid such a scenario
                         */}
-                        <FileUploadList clearable={false} showSize />
-                    </FileUploadRoot>
-                
-                    <Box w={'100%'}>
-                        {/* <Statistics /> */}
-                    </Box>
-                    <Box w={'100%'}>
-                        <FilesTable />
-                    </Box>
-                </VStack>
-            </Container>
+                                <FileUploadList clearable={false} showSize />
+                            </FileUploadRoot>
+
+                            <Box w={'100%'}>
+                                {/* <Statistics /> */}
+                            </Box>
+                            <Box w={'100%'}>
+                                <FilesTable />
+                            </Box>
+                        </VStack>
+                    </Container>
+                ) : (
+                    <Container h={'calc(100vh - 70px)'}>
+                        <Center h={'100%'}>
+                            <Stack>
+                                <Text>Connect wallet to upload files</Text>
+                                <ConnectWallet />
+                            </Stack>
+                        </Center>
+                    </Container>
+                )
+            }
         </>
     )
 }
