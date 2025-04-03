d72668c521ce4926bbac683fb87888d9c244989a
Arthur-Kamau <kennkamau09@gmail.com>
2025-04-03 19:02:11 +0300

    fix hash

--- web/src/utils/functions.ts
+++ web/src/utils/functions.ts
@@ -510,7 +510,7 @@ export const checkIfFileExistInUserFiles = async (file: File, files: ApiFileInfo
     let fileContent = await readFileContent(file)
     let aquafier = new Aquafier()
     let fileHash = aquafier.getFileHash(fileContent)
-    console.log(`file hash generated  ${fileHash} `)
+    console.log(`type of ${typeof(fileContent)} file hash generated  ${fileHash} `)
 
     // loop through all the files the user has 
     for (let fileItem of files) {
@@ -540,6 +540,7 @@ export const readFileContent = async (file: File): Promise<string | Uint8Array> 
         // If it's a text file, read as text
         return await readFileAsText(file);
     } else {
+        console.log("binary data....")
         // Otherwise for binary files, read as ArrayBuffer
         const res = await readFileAsArrayBuffer(file)
         return new Uint8Array(res);
