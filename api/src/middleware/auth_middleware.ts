e131ab66d8381d0c993b870b3d566595f53ca3ce
Arthur-Kamau <kennkamau09@gmail.com>
2025-04-09 17:29:49 +0300

    one more changes

--- api/src/middleware/auth_middleware.ts
+++ api/src/middleware/auth_middleware.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/database/db";
+import { prisma } from "../database/db";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export interface AuthenticatedRequest extends FastifyRequest {
