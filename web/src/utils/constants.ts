2bb0cb9904486a26ccfc5c9136a22182b430fd00
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-04-15 17:29:56 +0300

    Update required file size

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -7,7 +7,7 @@ export const SEPOLIA_SMART_CONTRACT_ADDRESS = "0x45f59310ADD88E6d23ca58A0Fa7A55B
 // export const await API_ENDPOINT() = `http://${import.meta.env.VITE_REMOTE || '127.0.0.1'}:${ import.meta.env.VITE_REMOTE_PORT || 3600}`;
 
 
-export const maxUserFileSizeForUpload = 1024 * 1024 * 1; // 1 GB in bytes
+export const maxUserFileSizeForUpload = 1024 * 1024 * 1000; // 1 GB in bytes
 export const maxFileSizeForUpload = 200 * 1024 * 1024; // 200MB in bytes
 
 export const SESSION_COOKIE_NAME = "pkc_nonce"
