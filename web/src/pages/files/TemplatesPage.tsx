901c651e2974bb529a7aa01ef18b9ba8348746fd
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-07-03 17:50:10 +0300

    Install few more components and update form template editor form

---
+++ web/src/pages/files/TemplatesPage.tsx
@@ -0,0 +1,12 @@
+import FormTemplateEditorShadcn from "@/components/aqua_forms/FormTemplateEditorShadcn"
+
+
+const TemplatesPage = () => {
+  return (
+    <div>
+        <FormTemplateEditorShadcn onSave={() => {}} />
+    </div>
+  )
+}
+
+export default TemplatesPage
