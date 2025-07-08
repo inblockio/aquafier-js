cdd554f874b723b14c3876a3c65857e8223aa432
Arthur-kamau <kennkamau09@gmail.com>
2025-07-08 10:52:01 +0300

    sign in page

--- web/src/layouts/NewShadcnLayoutWithSidebar.tsx
+++ web/src/layouts/NewShadcnLayoutWithSidebar.tsx
@@ -1,47 +1,62 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { ConnectWallet } from "@/components/ConnectWallet"
+import { ConnectWalletPage } from "@/components/ConnectWalletPage"
 import { Separator } from "@/components/shadcn/ui/separator"
 import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/shadcn/ui/sidebar"
+import appStore from "@/store"
 import { Bell, Users } from "lucide-react"
 import { Outlet } from "react-router-dom"
 import { Toaster } from "sonner"
+import { useStore } from "zustand"
 
 export default function NewShadcnLayoutWithSidebar() {
+    const { session } = useStore(appStore);
+
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b sticky top-0 z-50 bg-gray-50 w-full">
-                    <div className="flex items-center gap-2 px-3 w-full">
-                        <SidebarTrigger />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
-                        <div className="flex items-center space-x-4 ms-auto">
-                            <button className="p-2 text-gray-500 hover:text-gray-700">
-                                <Bell className="w-5 h-5" />
-                            </button>
-                            <button className="p-2 text-gray-500 hover:text-gray-700">
-                                <Users className="w-5 h-5" />
-                                {/* <span className="ml-1 text-sm">Invite members</span> */}
-                            </button>
-                            <button className="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700">
-                                Start free trial
-                            </button>
-                            {/* <div className="w-8 h-8 bg-red-500 rounded-full flex items-center justify-center text-white text-sm font-medium">
+        <>
+            {
+                session == null ? <>
+                    <ConnectWalletPage dataTestId="sign-in-pages" />
+                </>
+                    :
+
+
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset>
+                            <header className="flex h-16 shrink-0 items-center gap-2 border-b sticky top-0 z-50 bg-gray-50 w-full">
+                                <div className="flex items-center gap-2 px-3 w-full">
+                                    <SidebarTrigger />
+                                    <Separator orientation="vertical" className="mr-2 h-4" />
+                                    <div className="flex items-center space-x-4 ms-auto">
+                                        <button className="p-2 text-gray-500 hover:text-gray-700">
+                                            <Bell className="w-5 h-5" />
+                                        </button>
+                                        <button className="p-2 text-gray-500 hover:text-gray-700">
+                                            <Users className="w-5 h-5" />
+                                            {/* <span className="ml-1 text-sm">Invite members</span> */}
+                                        </button>
+                                        <button className="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700">
+                                            Start free trial
+                                        </button>
+                                        {/* <div className="w-8 h-8 bg-red-500 rounded-full flex items-center justify-center text-white text-sm font-medium">
                                 A
                             </div> */}
-                            <ConnectWallet dataTestId="sign-in-button-files-list" />
-                        </div>
-                    </div>
-                </header>
-                <div className="flex flex-1 flex-col gap-4 p-4">
-                    <Toaster position="top-right" richColors />
-                    <Outlet />
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
+                                        <ConnectWallet dataTestId="sign-in-button-files-list" />
+                                    </div>
+                                </div>
+                            </header>
+                            <div className="flex flex-1 flex-col gap-4 p-4">
+                                <Toaster position="top-right" richColors />
+                                <Outlet />
+                            </div>
+                        </SidebarInset>
+                    </SidebarProvider>
+            }
+        </>
     )
 }
