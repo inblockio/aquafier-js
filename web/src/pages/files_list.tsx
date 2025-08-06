1b28336d96d4caafe34a63c3d7120400c5e5aa69
Arthur-kamau <kennkamau09@gmail.com>
2025-08-06 11:05:25 +0300

    remove unecessary toast

--- web/src/pages/files_list.tsx
+++ web/src/pages/files_list.tsx
@@ -179,7 +179,7 @@ export default function FilesList() {
                                                       ? 'border-blue-500 text-blue-600'
                                                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                                                 }`}
-                                    >
+                                    > 
                                           All Files ({files.length})
                                     </button>
                                     {
--- web/src/pages/files_list_item.tsx
+++ web/src/pages/files_list_item.tsx
@@ -178,7 +178,7 @@ export default function FilesListItem({
                         </div>
                   </>
             )
-      }
+      } 
       const workFlowIdentityClaimActions = (workflowInfo: {
             isWorkFlow: boolean;
             workFlow: string;
