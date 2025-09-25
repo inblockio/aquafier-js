7305403bc384ef05f1c473af821c21da1d6d2ad3
Florian <Zusel7.Zusel7@gmail.com>
2025-09-25 16:40:58 +0200

    add workers
    Signed-off-by: Florian <Zusel7.Zusel7@gmail.com>

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   forbidOnly: false,
   retries: process.env.RETRIES ? parseInt(process.env.RETRIES) : 1,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['junit', { outputFile: 'results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
