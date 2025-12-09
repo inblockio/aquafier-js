d62034402a299dd55e9351c84647109d11f3421c
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-12-09 16:56:32 +0300

    Update tabs to remove some issue with file count

--- web/src/pages/files/files_list.tsx
+++ web/src/pages/files/files_list.tsx
@@ -308,7 +308,7 @@ export default function FilesList(filesListProps: FilesListProps) {
                                                                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                                                                   }`}
                                                       >
-                                                            {capitalizeWords(workflow.name.replace(/_/g, ' '))} ({workflow.count ?? 0})
+                                                            {capitalizeWords(workflow.name.replace(/_/g, ' '))} { workflow.name === 'aqua_files' ? '' : `(${workflow.count ?? 0})`}
                                                       </button>
                                                 )
                                           })
