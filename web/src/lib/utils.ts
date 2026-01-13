396a6675ecdc8b29b9ea5f2010c5adbd198a9251
Dalmas Ogembo <dalmasogembo@gmail.com>
2026-01-13 18:35:48 +0300

    fix encoding issues for text files

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
       return twMerge(clsx(inputs))
 }
+
+
+export function getCorrectUTF8JSONString(fileContent: any, tabs?: number) {
+      if(typeof fileContent === 'string') {
+            // It's already a string, return as is
+            const encoder = new TextEncoder() // TextEncoder always uses UTF-8
+            const utf8Bytes = encoder.encode(fileContent)
+            return utf8Bytes
+      }
+      let jsonContent = JSON.stringify(fileContent)
+      if (tabs) {
+            jsonContent = JSON.stringify(fileContent, null, tabs)
+      }
+      const encoder = new TextEncoder() // TextEncoder always uses UTF-8
+      const utf8Bytes = encoder.encode(jsonContent)
+      return utf8Bytes
+}
