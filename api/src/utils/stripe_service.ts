43d8290be959b79c6f5ffe4393ce24c41bf8e3f8
Dalmas Ogembo <dalmasogembo@gmail.com>
2026-02-02 11:38:34 +0300

    fix: Fix stripe setup issue

--- api/src/utils/stripe_service.ts
+++ api/src/utils/stripe_service.ts
@@ -3,7 +3,7 @@ import Logger from './logger';
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-12-15.clover',
+  apiVersion: '2026-01-28.clover',
   typescript: true,
 });
 
